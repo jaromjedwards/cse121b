1fd26cab11050c9d0809b826f4fa9cb7bb51db3f
Jarom Edwards <jaromjedwards@gmail.com>
2023-09-14 13:25:46 -0700

    week 1

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+'use strict';
+function sayHello() {
+    alert('Hello, World!');
+}
+  
+let firstName = "Antonia Francesca"
+
+let num1 = 100;
+let num2 = '10';
+let sum = num1 + parseInt(num2);
+console.log(sum)
